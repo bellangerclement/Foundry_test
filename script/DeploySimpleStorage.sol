a507f6d6f05301a24fec0cbe0827241b81ce91fa
Clement Bellanger <bellanger.consultant@gmail.com>
2023-06-27 20:57:50 +0200

    creation du script smart contract

---
+++ script/DeploySimpleStorage.sol
@@ -0,0 +1,14 @@
+import{ Script} from "forge-std/Script.sol";
+import { SimpleStorage} from "../src/SimpleStorage.sol";
+
+contract DeploySimpleStorage is Script {
+
+function run() external returns (SimpleStorage){
+
+    vm.startBroadcast();
+    SimpleStorage SimpleStorage= new SimpleStorage();
+    vm.stopBroadcast();
+    return simpleStorage
+}
+
+}
